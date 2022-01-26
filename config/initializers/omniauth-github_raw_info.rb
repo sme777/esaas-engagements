ffc677197ef160db4502ff40956d5e45ddc156a0
sme777 <samson.petrosyan8@gmail.com>
2022-01-25 17:44:10 -0800

    another try

--- config/initializers/omniauth-github_raw_info.rb
+++ config/initializers/omniauth-github_raw_info.rb
@@ -1,6 +1,6 @@
 require 'omniauth-oauth2'
 
-OmniAuth::Strategies::GitHub.class_eval do
+OmniAuth::Strategies::GitHub < OmniAuth::Strategies::OAuth2 do
   def raw_info
     access_token.options[:mode] = :header
     @raw_info ||= access_token.get('user').parsed
