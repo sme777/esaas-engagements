aeae15a4e59c258c6c7ae950ff06968cabbc1036
sme777 <samson.petrosyan8@gmail.com>
2022-01-25 17:34:38 -0800

    another try

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'] #Figaro.env.github_key, Figaro.env.github_secret, scope: 'user:email'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email' #Figaro.env.github_key, Figaro.env.github_secret, scope: 'user:email'
   unless Rails.env.production?
     OmniAuth.config.test_mode = true
     user = YAML.load(File.read "#{Rails.root}/db/github_mock_login.yml")[Rails.env]
