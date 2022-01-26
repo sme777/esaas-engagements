d5044bb674e79b279a0030e561c13ce018673887
sme777 <samson.petrosyan8@gmail.com>
2022-01-25 18:14:28 -0800

    add shits

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'] #Figaro.env.github_key, Figaro.env.github_secret, scope: 'user:email'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], {
+    # From: https://github.com/mkdynamic/omniauth-facebook/pull/174#issuecomment-300299724
+    # To fix: https://github.com/mkdynamic/omniauth-facebook/issues/284
+    token_params: { parse: :json },
+ } #Figaro.env.github_key, Figaro.env.github_secret, scope: 'user:email'
   unless Rails.env.production?
     OmniAuth.config.test_mode = true
     user = YAML.load(File.read "#{Rails.root}/db/github_mock_login.yml")[Rails.env]
